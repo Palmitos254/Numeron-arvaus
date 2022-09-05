bb42d9703b1a94fc876ce12348d62bfdd7a3ba1d
Palmito254 <bgr921@myy.haaga-helia.fi>
2022-09-05 18:30:39 +0300

    first

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState();
+  const [text2, setText2] = useState("Guess a number between 1-100");
+  const [num, setNum] = useState(Math.floor(Math.random() * 100) + 1);
+  const [kerrat, setKerrat] = useState(1);
+
+  const buttonPressed = () => {
+    setKerrat(kerrat + 1)
+    if (num == text) {
+      Alert.alert('You guessed the number in ' + kerrat + " guesses");
+      setKerrat(1)
+      setNum(Math.floor(Math.random() * 100) + 1)
+      setText2("Guess a number between 1-100")
+    } else if (num < text) {
+      setText2("Your guess " + text + " is too high")
+    } else {
+      setText2("Your guess " + text + " is too low")
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text2}</Text>
+      <Text></Text>
+      <TextInput
+        style={{width:200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={text => setText(text)}  
+        value={text}
+      />
+      <Text></Text>
+      <Button onPress={buttonPressed} title="Make a Guess" />
       <StatusBar style="auto" />
     </View>
   );
